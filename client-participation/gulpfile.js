19ecd2936b3f5fc42f5a89a1cd076aa7296e78ac
Patrick Abi Salloum <patrick.abisalloum@gmail.com>
2020-08-31 08:40:19 +0300

    Manually cherry picked sari's changes Modifying gulp to serve service-worker.js from the root of the domain

--- client-participation/gulpfile.js
+++ client-participation/gulpfile.js
@@ -243,6 +243,10 @@ gulp.task('embedJs', function() {
   .pipe(gulp.dest(destRootBase));
 });
 
+gulp.task('notificationServiceWorker', function() {
+  return gulp.src(['api/service-worker.js']).pipe(gulp.dest(destRootBase));
+});
+
 gulp.task('index', [
   'sparklines',
 ], function() {
@@ -539,7 +543,6 @@ gulp.task("scriptsOther", function() {
     files.push('bower_components/d3/d3.min.js');
   }
   files.push('d3.v4.min.js');
-  files.push('service-worker.js');
   var s = gulp.src(files);
   if (!devMode) {
     s = s
@@ -554,7 +557,6 @@ gulp.task("scriptsTemp", function() {
 
   var files = [];
   files.push('d3.v4.min.js');
-  files.push('service-worker.js');
   var s = gulp.src(files);
   if (!devMode) {
     s = s
@@ -620,6 +622,7 @@ gulp.task('common', [
   "fontawesome",
   "index",
   "embedJs",
+  "notificationServiceWorker",
   ], function() {
     showDesktopNotification("BUILD UPDATED", "woohoo");
 });
@@ -958,6 +961,22 @@ function deploy(uploader) {
       logStatement: function(file) {
         console.log("upload path cached_embedJs_"+embedJsCacheSeconds+" /embedPreprod.js");
         return "/embedPreprod.js";
+      },
+      subdir: null,
+    }));
+
+    // service-worker.js
+    var serviceWorkerCacheSeconds = 60;
+    promises.push(deployBatch({
+      srcKeep: destRootBase + '/**/service-worker.js',
+      headers: {
+        'x-amz-acl': 'public-read',
+        'Content-Type': 'application/javascript',
+        'Cache-Control': 'no-cache'.replace(/MAX_AGE/g, serviceWorkerCacheSeconds),
+      },
+      logStatement:  function(file) {
+        console.log("upload path cached_serviceWorker_"+serviceWorkerCacheSeconds+" /service-worker.js");
+        return "/service-worker.js";
       },
       subdir: null,
     }));
